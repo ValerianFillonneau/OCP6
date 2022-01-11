4402a6b4bac7c86c5bf327cef96adb99e6260433
ValerianFillonneau <leleva14@gmail.com>
2022-01-11 17:15:56 +0100

    correctif image

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -7,7 +7,7 @@ exports.createSauce = (req, res) => {
     delete sauceObject._id;
     const sauce = new Sauce({
         ...sauceObject,
-        imageUrl: `${req.protocole}://${req.get('host')}/images/${req.file.filename}`
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
     sauce.save()
     .then(() => res.status(201).json({message: 'Sauce enregistrée !'}))
@@ -18,7 +18,7 @@ exports.modifySauce = (req, res, next) => {
     const sauceObject = req.file ?
     {
         ...JSON.parse(req.body.sauce),
-        imageUrl: `${req.protocole}://${req.get('host')}/images/${req.file.filename}`
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     } : {...req.body};
     Sauce.updateOne({_id: req.params.id}, {...sauceObject, _id: req.params.id})
     .then(() => res.status(200).json({message: 'Sauce modifiée !'}))
@@ -49,3 +49,14 @@ exports.getAllSauces = (req, res, next) => {
     .then(sauces => res.status(200).json(sauces))
     .catch(error => res.status(400).json({error}));
 };
+
+exports.likeSauce = (req, res, next) => {
+    const like = [];
+    const dislike = [];
+    Sauce.findOne({_id: req.params.id})
+    .then(() => {
+        if (!like) {
+
+        }
+    })
+}
