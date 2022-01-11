40b329bf7223b25a605ff0bd03198e50bdc579a7
ValerianFillonneau <leleva14@gmail.com>
2022-01-11 15:41:48 +0100

    routes files

---
+++ routes/sauces.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.router();
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+const saucesCtrl = require('../controllers/sauces');
+
+router.post('/', auth, multer, saucesCtrl.createSauce);
+router.put('/:id', auth, multer, saucesCtrl.mofifySauce);
+router.delete('/:id', auth, saucesCtrl.deleteSauce);
+router.get('/:id', auth, saucesCtrl.getOneSauce);
+router.get('/', auth, saucesCtrl.getAllSauces);
+router.post('/:id/like', auth, saucesCtrl.likeSauce);
+
+module.exports = router;
+
+
